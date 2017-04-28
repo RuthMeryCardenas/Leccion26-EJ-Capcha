dd40a0ae92004fcfa7bbffafba4f462c9bb114ce
RuthMery <ruth.cardenas.perez@gmail.com>
2017-04-28 13:17:27 -0500

    index-generador captcha

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+window.addEventListener("load",function () {
+  var valorCaptchaInput = document.getElementById("valor-captcha");
+  var confirmCaptchaInput = document.getElementById("confirm-captcha");
+  var setCaptchaButton = document.getElementById("setCaptcha");
+  var reloadCaptchaButton = document.getElementById("reloadCaptcha");
+  var resultsInput = document.getElementById("results");
+  valorCaptchaInput.value = randomCaptcha();
+
+  reloadCaptchaButton.addEventListener("click", function () {
+    valorCaptchaInput.value = randomCaptcha();
+  })
+  setCaptchaButton.addEventListener("click", function () {
+    var a = valorCaptchaInput.value;
+    var b = confirmCaptchaInput.value;
+    if (a = b) {
+      resultsInput.value = "ok";
+    }else {
+      resultsInput.value = "error";
+    }
+  });
+});
+function randomCaptcha() {
+  var captchaValues = ["A1g2RT@", "FgIo45p", "Ghty70Q"];
+  return captchaValues[Math.floor(Math.random() * captchaValues.length) + 0];
+}
